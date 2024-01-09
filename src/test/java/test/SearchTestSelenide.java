ec2fcdefe87d49e1e3b6b5e914229654500b359e
Maksim Atenyaev <129950558+maxaten@users.noreply.github.com>
2024-01-09 21:29:30 +0600

    add test

--- src/test/java/test/SearchTestSelenide.java
+++ src/test/java/test/SearchTestSelenide.java
@@ -1,5 +1,6 @@
 package test;
 
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
@@ -10,11 +11,13 @@ import static io.appium.java_client.AppiumBy.*;
 import static io.qameta.allure.Allure.step;
 
 
+@Tag("android")
 public class SearchTestSelenide extends TestBase{
 
 
     String name1 = "Appium";
     String name2 = "Batman";
+
     @Test
     void successfulSearchTest()  {
         step("Кликнуть по полю поиска",() -> {
