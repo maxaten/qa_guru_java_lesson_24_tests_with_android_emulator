e3c0238af1a1547d9b6bd003bbe67ba0c5298a4b
Maksim Atenyaev <129950558+maxaten@users.noreply.github.com>
2024-01-20 01:47:56 +0600

    Divide the tests by business logic

---
+++ src/test/java/test/OnbordingScreenTest.java
@@ -0,0 +1,56 @@
+package test;
+
+import io.qameta.allure.Feature;
+import io.qameta.allure.Issue;
+import io.qameta.allure.Owner;
+import io.qameta.allure.Story;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import pages.OnboardingPage;
+
+import static io.qameta.allure.Allure.step;
+
+
+public class OnbordingScreenTest extends TestBase {
+
+    private OnboardingPage onboardingPage = new OnboardingPage();
+
+    @Test
+    @Owner("Bruce")
+    @Story("Wikia")
+    @Feature("Онбординг")
+    @Tag("emulator")
+    @Issue("HOMEWORK-1043")
+    @DisplayName("Проверка текстов на экранах онбординга")
+    void checkOnboardingTest() {
+        step("Проверка первого экрана", () -> {
+            onboardingPage.checkPrimaryText("The Free Encyclopedia …in over 300 languages")
+                    .checkSecondaryText("We’ve found the following on your device:")
+                    .checkOptionLabel("1. English")
+                    .checkLanguageButtonText("Add or edit languages")
+                    .continueButtonClick();
+        });
+        step("Проверка второго экрана", () -> {
+            onboardingPage.checkPrimaryText("New ways to explore")
+                    .checkSecondaryText("Dive down the Wikipedia rabbit hole with a constantly updating " +
+                            "Explore feed. Customize the feed to your interests – whether it’s learning about historical " +
+                            "events On this day, or rolling the dice with Random.")
+                    .continueButtonClick();
+        });
+        step("Проверка третьего экрана", () -> {
+            onboardingPage.checkPrimaryText("Reading lists with sync")
+                    .checkSecondaryText("You can make reading lists from articles you want to read later, " +
+                            "even when you’re offline. Login to your Wikipedia account to sync your reading lists. " +
+                            "Join Wikipedia")
+                    .continueButtonClick();
+        });
+        step("Проверка четвертого экрана", () -> {
+            onboardingPage.checkPrimaryText("Send anonymous data")
+                    .checkSecondaryText("Help make the app better by letting us know how you use it. " +
+                            "Data collected is anonymous. Learn more")
+                    .checkRejectButtonText("Reject")
+                    .checkAcceptButtonText("Accept");
+        });
+    }
+}
---
+++ src/test/java/test/SearchTest.java
@@ -0,0 +1,83 @@
+package test;
+
+import io.qameta.allure.Feature;
+import io.qameta.allure.Issue;
+import io.qameta.allure.Owner;
+import io.qameta.allure.Story;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Tags;
+import org.junit.jupiter.api.Test;
+import pages.RequestInformationPage;
+import pages.SearchPage;
+import pages.SearchResultPage;
+
+import static com.codeborne.selenide.Selenide.*;
+import static io.qameta.allure.Allure.step;
+
+
+public class SearchTest extends TestBase {
+
+
+    private SearchPage searchPage = new SearchPage();
+    private SearchResultPage searchResultPage = new SearchResultPage();
+    private RequestInformationPage informationPage = new RequestInformationPage();
+
+
+    String name1 = "Appium";
+
+
+    @Test
+    @Owner("Bruce")
+    @Story("Wikia")
+    @Feature("Поиск")
+    @Issue("HOMEWORK-1043")
+    @DisplayName("Проверка поиска")
+    @Tags({@Tag("regress"), @Tag("emulator")})
+    void successfulSearchTest() {
+
+
+        back();
+        step("Кликнуть по полю поиска", () -> {
+            searchPage.clickOnSearch();
+        });
+
+        step(String.format("Вставить значение %s", name1), () -> {
+            searchPage.clickOnSecondSearch(name1);
+        });
+
+        step("Проверка искомого запроса", () -> {
+            searchResultPage.checkElementInList();
+        });
+    }
+
+    @Test
+    @Owner("Bruce")
+    @Story("Wikia")
+    @Feature("Поиск")
+    @Issue("HOMEWORK-1043")
+    @DisplayName("Проверка искомого запроса в статье")
+    @Tag("emulator")
+    void successfulSearchAndClickTest() {
+
+        String name2 = "Batman";
+
+        back();
+        step("Кликнуть по полю поиска", () -> {
+            searchPage.clickOnSearch();
+        });
+
+        step(String.format("Вставить значение %s", name2), () -> {
+            searchPage.clickOnSecondSearch(name2);
+        });
+
+        step(String.format("Выбрать элемент %s в списке", name2), () -> {
+            searchResultPage.clickOnFirstSearch();
+        });
+
+        step(("Искомый запрос в статье присутствует"), () -> {
+            informationPage.checkVisible()
+                    .checkText(name2);
+        });
+    }
+}
